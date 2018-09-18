8561413533aca81f1f001c852515f55901db0b93
binartist <mi.zhou@innovaee.com.cn>
2018-09-18 11:34:28 +0800

    add pop moudule to export

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from './Controller/UIViewController';
 export * from './View/UIWindow';
 export * from './View/UIView';
 export * from './Application/UIApplication';
+export * from './Popup/UIPopup';
